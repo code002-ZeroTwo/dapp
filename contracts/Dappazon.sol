51eeab35819a18247346dadb4eded0d672e45628
siddhartha <siddharthpaudel41@gmail.com>
2023-06-27 00:25:16 +0545

    solidity

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,90 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+  // code goes here... 
+  address public owner;
+
+  struct Item {
+      uint256 id; 
+      string name;
+      string category;
+      string image;
+      uint256 cost;
+      uint256 rating;
+      uint256 stock;
+  }
+
+  struct Order {
+    uint256 time; 
+    Item item;
+  }
+
+  mapping(uint256 => Item) public items;
+  mapping(address => uint256) public orderCount;
+  mapping(address => mapping(uint256 => Order)) public orders;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  constructor () {
+    owner = msg.sender;
+  }
+
+  event List(string name, uint256 cost,uint256 quantity);
+
+  // list products 
+  function list(
+        uint256 _id, 
+        string memory _name, 
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+  )public onlyOwner{
+    require(msg.sender == owner);
+    // code goes here... 
+    
+    // create Item struct
+    Item memory item = Item(_id, _name,_category,_image,_cost,_rating,_stock);
+    
+    // save the item to blockchain
+    items[_id] = item;
+
+    // Emit an event
+    emit List(_name, _cost, _stock);
+    
+  }
+
+
+  // buy products 
+  function buy(uint256 _id) public payable{
+    // Receive crypto
+
+    // Fetch iitem
+    Item memory item = items[_id];
+
+    // Create an order
+    Order memory order = Order(block.timestamp , item);
+
+
+    // Add order for user
+    orderCount[msg.sender]++;
+    orders[msg.sender][orderCount[msg.sender]] = order;
+
+
+    // Substact stock
+    items[_id].stock = item.stock -1;
+
+    // Emit event
+
+  }
+
+
+  // withdraw  funds 
+}
+
+
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,68 @@
-const { expect } = require("chai")
+// automated testing for smartcontract
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ID = 1
+const Name = "Shoes"
+const Category = "clothing"
+const Image = "image"
+const cost = tokens(1);
+const rating = 4
+const stock = 5
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
 
-})
+  beforeEach(async () => {
+    [deployer , buyer] = await ethers.getSigners();
+
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    it("sets the owner" , async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+
+  });
+
+  describe("Listing", () => {
+    let transaction
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(
+        ID, Name,Category,Image,cost,rating,stock
+      )
+      await transaction.wait()
+
+      transaction = await dappazon.connect(buyer).buy(ID,{value:cost})
+    })
+
+    it("Returns item attributes" , async () => {
+    const item = await dappazon.items(ID);
+    expect(item.id).to.equal(ID);
+    expect(item.name).to.equal(Name);
+    expect(item.category).to.equal(Category);
+    expect(item.image).to.equal(Image);
+    expect(item.cost).to.equal(cost);
+    expect(item.rating).to.equal(rating);
+    expect(item.stock).to.equal(stock);
+    })
+
+    it("emits the List event" , () => {
+      expect(transaction).to.emit(dappazon,"List")
+    }) 
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address)
+      expect(result).to.equal(cost)
+    })
+
+  });
+});
